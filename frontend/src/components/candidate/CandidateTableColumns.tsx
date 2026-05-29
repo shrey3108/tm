07fee97bc4e42fca6052a2aae38e3c12f80f24bb
utsav-dhimmar <utsavdhimmar987@gmail.com>
2026-05-29 16:23:42 +0530

    fix: wrap email address

--- frontend/src/components/candidate/CandidateTableColumns.tsx
+++ frontend/src/components/candidate/CandidateTableColumns.tsx
@@ -73,7 +73,7 @@ export const useCandidateTableColumns = <T extends UnifiedCandidate>({
             `${c.first_name || ""} ${c.last_name || ""}`.trim() ||
             "Unknown Candidate";
           return (
-            <div className="flex flex-col gap-0.5 max-w-[160px] ">
+            <div className="flex flex-col gap-0.5 ">
               <span
                 className="text-foreground block capitalize"
               >
@@ -85,10 +85,10 @@ export const useCandidateTableColumns = <T extends UnifiedCandidate>({
                   toTitleCase(fullName)
                 )}
               </span>
-              <span className="text-muted-foreground w-full break-all">
+              <span className="text-muted-foreground text-wrap break-all w-full">
                 {c.email || "N/A"}
               </span>
-              <span className="text-muted-foreground w-full break-all">
+              <span className="text-muted-foreground text-wrap w-full break-all">
                 {c.phone || "N/A"}
               </span>
             </div>
